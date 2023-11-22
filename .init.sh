c15ba911eaf3279472c2405ecd211710113b3ff2
Fabrice Ducos <fabrice.ducos@univ-lille.fr>
2023-11-22 17:01:53 +0100

    .init.sh updated

--- .init.sh
+++ .init.sh
@@ -2,13 +2,16 @@
 
 default_name=myproject
 
+
 echo -n "Name of the project without space)? "
 read project
+
+# hyphens are not allowed in Python modules, replace them with underscores
+project=`echo $project | tr - _`
 
 for input_file in setup.cfg Makefile pyproject.toml
 do
   sed -i '' "s/$default_name/$project/g" $input_file
 done
 
-mv -v $default_name $project
-
+mv -v $default_name $module_name
