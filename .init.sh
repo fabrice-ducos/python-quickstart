aad42f577548f9a0446a5df17c35d65365058601
Fabrice Ducos <fabrice.ducos@univ-lille.fr>
2023-11-27 17:40:43 +0100

    .init.sh and pyproject.toml updated with author and author's email

--- .init.sh
+++ .init.sh
@@ -6,12 +6,20 @@ default_name=myproject
 echo -n "Name of the project without space)? "
 read project
 
+echo -n "Author's name (e.g. John Doe)? "
+read author
+
+echo -n "Author's email (e.g. john.doe@example.com)? "
+read email
+
 # hyphens are not allowed in Python modules, replace them with underscores
 project=`echo $project | tr - _`
 
 for input_file in setup.cfg Makefile pyproject.toml
 do
-  sed -i '' "s/$default_name/$project/g" $input_file
+  sed -i '' "s/$default_name/$project/g
+s/__author__/$author/g
+s/__email__/$email/g" $input_file
 done
 
 mv -v $default_name $project
