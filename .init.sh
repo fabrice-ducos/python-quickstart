cc30736e1b57721e403aab5fc80cc3f8a8f626af
Fabrice Ducos <fabrice.ducos@univ-lille.fr>
2023-11-28 10:42:27 +0100

    .init.sh: updated with default values

--- .init.sh
+++ .init.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-default_name=myproject
-
+default_project=myproject
+default_author="John Doe"
+default_email="john.doe@example.com"
 
 echo -n "Name of the project without space)? "
 read project
+project=${project:-$default_project}
 
-echo -n "Author's name (e.g. John Doe)? "
+echo -n "Author's name (e.g. $default_project)? "
 read author
+author=${author:-$default_author}
 
-echo -n "Author's email (e.g. john.doe@example.com)? "
+echo -n "Author's email (e.g. $default_email)? "
 read email
+email=${email:-$default_email}
 
 # hyphens are not allowed in Python modules, replace them with underscores
 project=`echo $project | tr - _`
 
 for input_file in setup.cfg Makefile pyproject.toml tests/tests.py
 do
-  sed -i '' "s/$default_name/$project/g
-s/__author__/$author/g
-s/__email__/$email/g" $input_file
+  sed -i '' "s/$default_project/$project/g
+             s/__author__/$author/g
+             s/__email__/$email/g" $input_file
 done
 
-mv -v $default_name $project
+mv -v $default_project $project
